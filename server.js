73f12b7aa674dc9ed73c2d0dc3b3a1264be3b931
hiheecho <jhh7929@gmail.com>
2022-08-12 23:40:35 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use (express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT,()=>{
+    console.log('server listening ' + PORT);
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'views','main.html'))
+})
