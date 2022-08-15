2106a54147cef733feeab7c000062cab99b95622
hiheecho <jhh7929@gmail.com>
2022-08-16 02:20:06 +0900

    menu design and slide update

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.listen(PORT,()=>{
 })
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'views','index.html'))
+    res.sendFile(path.join(__dirname, 'views','./index.html'))
 })
